51ec45600958eef1fc66ca4c7950034c0b624992
tony capone <87581644+TonyCapon3@users.noreply.github.com>
2022-07-26 11:35:34 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -17,26 +17,6 @@ RegisterNetEvent('tc-vending-chargeDrink', function(drink, cash, label)
 
 end)
 
--- RegisterNetEvent('tc-vending-chargeDrink', function(drink, cash)
---     local src = source
---     local Player = QBCore.Functions.GetPlayer(src)
---     local current = args.type
---     local drink = Config.Shop[current].drinkName
---     local cash = Config.Shop[current].drinkPrice
---     local label = Config.Shop[current].drinkLabel
-
---         if Player.Functions.GetMoney('cash') >= cash then
---            Player.Functions.AddItem(drink, 1)
---            TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[drink], "add")
---            Player.Functions.RemoveMoney('cash', cash)
---            TriggerClientEvent('QBCore:Notify', src, 'Bought ' ..label.. ' for $' ..cash, 'success')
---            TriggerClientEvent('tc-vending-buyDrink')
---         else
---             TriggerClientEvent('QBCore:Notify', src, 'You Dont Have Enough Cash You Need $' ..cash, 'error')
---         end
-
--- end)
-
 
 QBCore.Functions.CreateUseableItem("prime", function(source, item)
     local Player = QBCore.Functions.GetPlayer(source)
